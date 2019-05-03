09923af4824ec6f664f38e62624bd238e0f01563
Steven Xie <dev@stevenxie.me>
2019-05-03 12:55:16 -0400

    [depr] Spice up CLI with 'urfave/cli' (make it nice)

--- cmd/depression/main.go
+++ cmd/depression/main.go
@@ -7,30 +7,80 @@ import (
 	"io"
 	"os"
 
-	flag "github.com/spf13/pflag"
 	"github.com/stevenxie/utils/depression"
+	"gopkg.in/urfave/cli.v1"
 )
 
 func main() {
-	// Define and parse flags.
-	decode := flag.BoolP("decode", "d", false, "Decode from stdin.")
-	flag.Parse()
+	app := cli.NewApp()
+	app.Usage = "Encode and decode using depression representation."
+	app.UsageText = "depression [options] < (input) > (output)\n" +
+		"   depression [options] (file)"
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "decode,d",
+			Usage: "Decode from stdin.",
+		},
+		cli.BoolFlag{
+			Name:   "help,h",
+			Hidden: true,
+		},
+	}
 
-	// Either encode or decode from stdin.
-	var translator io.Writer
-	if *decode {
+	// Configure app.
+	app.Author = "Steven Xie <hello@stevenxie.me>"
+	app.Action = run
+	app.HideVersion = true
+	app.HideHelp = true
+	app.EnableBashCompletion = true
+
+	// Run and exit with appropriate code.
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(c *cli.Context) error {
+	if c.Bool("help") {
+		cli.ShowAppHelp(c) // show help and exit
+		return nil
+	}
+	args := c.Args()
+	if len(args) > 1 {
+		fmt.Fprintf(os.Stderr, "Error: expected 0 or 1 arguments, received %d.\n\n",
+			len(args))
+		cli.ShowAppHelpAndExit(c, 1)
+	}
+
+	// Either encode or decode.
+	var (
+		translator io.Writer
+		decode     = c.Bool("decode")
+	)
+	if decode {
 		translator = depression.NewDecoder(os.Stdout)
 	} else {
 		translator = depression.NewEncoder(os.Stdout)
 	}
 
-	// Perform translation.
-	if _, err := io.Copy(translator, os.Stdin); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v", err)
-		os.Exit(1)
+	// Either use stdin or read a file.
+	var src io.Reader
+	if len(args) == 0 {
+		src = os.Stdin
+	} else {
+		var err error
+		if src, err = os.Open(args[0]); err != nil {
+			return err
+		}
 	}
 
-	if !*decode {
+	// Perform translation.
+	if _, err := io.Copy(translator, src); err != nil {
+		return err
+	}
+	if !decode {
 		fmt.Println() // append newline for formatting
 	}
+	return nil
 }
